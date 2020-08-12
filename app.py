e220daa296e2d3d85211348a127f9ca27a7f5a65
AymanSulaiman <saymansulaiman@gmail.com>
2020-08-11 22:00:45 -0700

    still formulating ideas with streamlit

--- app.py
+++ app.py
@@ -4,12 +4,41 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import os
+import seaborn as sns
 
 import time
 
 st.title('Superconductor Analysis and Machine Learning')
 
 
-path = os.path.join('merged.csv')
-df = pd.read_csv(path)
-df
+path_merged = os.path.join('merged.csv')
+path_train = os.path.join('train.csv')
+path_unique_m = os.path.join('unique_m.csv')
+
+df_merged = pd.read_csv(path_merged)
+df_train = pd.read_csv(path_train)
+df_unique_m = pd.read_csv(path_unique_m)
+
+
+@st.cache(suppress_st_warning=True)
+def show_dataframe(x):
+    if x == 'm':
+        return df_merged
+    elif x == 't':
+        return df_train
+    elif x == 'u':
+        return df_unique_m
+    else:
+        return 'you done goofed'
+
+st.title('train')
+st.write(show_dataframe(x = 't'))
+
+st.title('unique')
+st.write(show_dataframe(x = 'u'))
+
+st.title('merged')
+st.write(show_dataframe(x = 'm'))
+
+sns.distplot(x = show_dataframe(x = 't')['critical_temp'])
+st.pyplot()
