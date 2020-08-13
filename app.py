e1dde7e2f33b5b5493e97ce0fafaf7469d9f0987
AymanSulaiman <saymansulaiman@gmail.com>
2020-08-12 22:33:23 -0700

    Made a new dataset and I have exported XGBoost models

--- app.py
+++ app.py
@@ -6,17 +6,41 @@ import pandas as pd
 import os
 import plotly.express as px
 import plotly.graph_objects as go
+from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
+from sklearn.metrics import r2_score
 
-st.title('Superconductor Analysis and Machine Learning')
+st.title('Superconductor Analysis and Machine Learning App')
+
+st.write('''
+This is an app that predicts the Critical Temperature of a Superconductor 
+using XGBoost.  Shoutout to UC Irvine for providing the clean data 
+so I can make this app. Here is the link to the [dataset]('https://archive.ics.uci.edu/ml/datasets/Superconductivty+Data')
+and the [Data Professor]('') who's tutorial I followed for the streamlit app.  
+''')
+
+st.sidebar.header('User Input Features')
 
 
 path_merged = os.path.join('data','merged.csv')
 path_train = os.path.join('data','train.csv')
 path_unique_m = os.path.join('data','unique_m.csv')
+path_data_no_elements = os.path.join('data','data_no_elements')
+
 
 df_merged = pd.read_csv(path_merged)
 df_train = pd.read_csv(path_train)
 df_unique_m = pd.read_csv(path_unique_m)
+df_data_no_elements = pd.read_csv(path_data_no_elements)
+
+
+X = df_merged.drop(['critical_temp', 'material'], axis=1)
+y = df_merged['critical_temp'].values.reshape(-1,1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+regressor = XGBRegressor()
+regressor.load_model('model.txt')
 
 
 # @st.cache(suppress_st_warning=True)
@@ -46,8 +70,8 @@ df_unique_m = pd.read_csv(path_unique_m)
 # st.pyplot()
 
 
-x = df_merged.head(30)
-x
+df_merged_head_30 = df_merged.head(30)
+df_merged_head_30
 
 st.title(f'Number of rows: {len(df_merged)}')
 
@@ -58,7 +82,8 @@ fig = px.scatter(
     y='critical_temp',
     size='critical_temp', 
     color='number_of_elements',
-    width=1200, height=800
+    width=1000, 
+    # height=800
 )
 
 
@@ -76,7 +101,20 @@ fig.update_layout(
 st.plotly_chart(fig)
 
 st.write('''
-# hello world
+## The Machine Learning Algorithm
+''')
 
-This is a blah blah blah.
+X = df_merged.drop(['critical_temp', 'material'], axis=1)
+y = df_merged['critical_temp'].values.reshape(-1,1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+regressor = XGBRegressor()
+regressor.load_model('model.txt')
+
+y_test_pred = regressor.predict(X_test)
+score_test = r2_score(y_test, y_test_pred)
+
+st.write(f'''
+Score Test: {100*round(score_test,4)} %
 ''')
